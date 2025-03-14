bb4bf0a5c469fe49d823ba13fbe41ea155213492
katie13255 <katie.p050905@gmail.com>
2025-03-14 17:17:19 -0400

    drop down menu
    Added drop down menu code to main

--- Closet/closetHome.php
+++ Closet/closetHome.php
@@ -2,6 +2,7 @@
 session_start();
 ?>
 
+
 <html>
 <head>
     <title>Your Closet</title>
@@ -9,16 +10,45 @@ session_start();
 </head>
 <body>
 
+
 <p>This is your closet</p>
 
+
 <?php
+   
 require "../db.php";
 
+
+    $categories = categorize_clothes();
+
+
+    $categoryNames = array("Headwear", "Top", "Outerwear", "Bottom", "Footwear", "Dress", "Accessories")
+    foreach ($categoryNames as $cNames) {
+         echo "<details>";
+         echo "<summary>";
+         echo $cName;
+         echo "</summary>";  
+       
+        $items = get_items($row['name']);
+
+
+        foreach ($items as $item) {
+            echo "<img src='../ClothingImages/" .$item["image"] . ".jpg' alt='" . htmlspecialchars($item["cName"]) . "' style='width:100px;height:100px;'>";
+            echo "<br>";
+            echo "<p>" . htmlspecialchars($item["cName"]) . "</p>";
+            echo "<br>";
+            echo "<p>" . htmlspecialchars($item["category"]) . "</p>";
+        }
+        echo "</details>";
+    }
+   
+   
 if (!isset($_SESSION["username"])) {
     header("LOCATION:../login.php");
     exit; // Important: Stop further execution after redirect
 }
 ?>
+
 
 <form action="addClothingItem.php" method="post">
     <p align="left">
@@ -26,24 +56,23 @@ if (!isset($_SESSION["username"])) {
     </p>
 </form>
 
+
 <p>Closet items</p>
+
 
 <?php
 $dbh = connectDB();
+
 
 $stmt = $dbh->prepare("SELECT * FROM clo_clothing_items WHERE username = :username");
 $stmt->bindParam(':username', $_SESSION["username"]);
 $stmt->execute();
 $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($items as $item) {
-    echo "<img src='../ClothingImages/" .$item["id"] . ".jpg' alt='" . htmlspecialchars($item["cName"]) . "' style='width:100px;height:100px;'>";
-    echo "<br>";
-    echo "<p>" . htmlspecialchars($item["cName"]) . "</p>";
-    echo "<br>";
-    echo "<p>" . htmlspecialchars($item["category"]) . "</p>";
-}
+
 ?>
+
 
 </body>
 </html>
+
