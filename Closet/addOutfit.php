18c52bdab2686813019573c36813ce0626b1da18
Logan Cole <lacole@mtu.edu>
2025-03-17 14:36:57 -0400

    Update addOutfit.php
    not finished yet

--- Closet/addOutfit.php
+++ Closet/addOutfit.php
@@ -4,10 +4,32 @@ session_start();
 
 <html>
 <p>Create an outfit</p>
-</html>
+
 
 <?php
 if (!isset($_SESSION["username"])) {
 	header("LOACTION:../index.php");
 }
 ?>
+
+
+<form action="addOutfit.php" method="post">
+    <label for="outfitName">Outfit Name:</label>
+    <input type="text" id="outfitName" name="outfitName">
+    <br>
+
+    <label for="outfitImage">Image:</label>
+    <input type="file" id="item" name="item" accept="image/*">
+    <br>
+
+    
+
+
+    <input type="submit" value="Add Outfit" name="addOutfit">
+    <br>
+
+
+
+
+
+</html>
