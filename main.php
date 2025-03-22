0dbb6bea9d38fcd1038e6fe695b4cb32672d8842
Fleser11 <97135604+Fleser11@users.noreply.github.com>
2025-03-22 15:26:59 -0400

    fixed css

--- main.php
+++ main.php
@@ -4,38 +4,30 @@ session_start();
 
 <html>
 
-<head>
-	<link rel="stylesheet" href="styles/style.css">
-</head>
+	<head>
+		<link rel="stylesheet" href="style.css">
+		<title>Main Page</title>
+	</head>
 
-<p>Welcome to our the main page.</p>
+	<body>
 
 <?php
 require "db.php";
 
 if (!isset($_SESSION["username"])) {
 	header("LOCATION:index.php");
-} else {
-	echo '<p align="right"> Welcome '. $_SESSION["username"].'</p>';
+} else { 
+	echo '<div id="logout">'. $_SESSION["username"].' <a href="index.php"><button>Logout</button></div></a>';
 ?>
+	<div id="center-box">
+		<h1>Welcome!</h1>
+		<div id="login-box">
 
-<form action="index.php" method="post">
-	<p align="right">
-		<input type="submit" value="logout" name="logout">
-	</p>
-</form>
-
-<form action="Closet/closetHome.php" method = "post">
-	<p align="center">
-		<input type="submit" value="View Closet" name="closet">
-	</p>
-</form>
-
-<form action="Outfits/outfitHome.php" method = "post">
-	<p align="center">
-		<input type="submit" value="View Outfits" name="outfits">
-	</p>
-
+<a href="Closet/closetHome.php"><button class="button">Closet</button></a>
+<a href="Outfits/outfitHome.php"><button class="button">Wardrobe</button></a>
+</div></div>
+</body>
+</html>
 <?php
 }
 ?>
