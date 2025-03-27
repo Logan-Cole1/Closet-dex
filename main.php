987576346cb90e6b021085e1acbda62f51d247c2
Logan Cole <lacole@mtu.edu>
2025-03-26 20:43:34 -0400

    Fixed CSS for logout button

--- main.php
+++ main.php
@@ -17,15 +17,23 @@ require "db.php";
 if (!isset($_SESSION["username"])) {
 	header("LOCATION:index.php");
 } else { 
-	echo '<div id="logout">'. $_SESSION["username"].' <form action="index.php" method="post"><input type="submit", value="Logout" name="logout"></div></form>';
 ?>
+	<div id="logout">
+	<?php echo $_SESSION["username"]; ?>
+	<form action="index.php" method="post">
+		<button class="button" type="submit" name="logout">Logout</button>
+	</form>
+	</div>
+
+	
 	<div id="center-box">
 		<h1>Welcome!</h1>
 		<div id="login-box">
 
-<a href="Closet/closetHome.php"><button class="button">Closet</button></a>
-<a href="Outfits/outfitHome.php"><button class="button">Wardrobe</button></a>
-</div></div>
+			<a href="Closet/closetHome.php"><button class="button">Closet</button></a>
+			<a href="Outfits/outfitHome.php"><button class="button">Wardrobe</button></a>
+		</div>
+	</div>
 </body>
 </html>
 <?php
