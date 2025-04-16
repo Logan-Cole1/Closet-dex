45304ac8cec4d1d2cf62f00c48c06b4d95435d39
McCoy Ziehl <mvziehl@mtu.edu>
2025-04-16 13:21:31 -0400

    Add icon in title

--- main.php
+++ main.php
@@ -17,6 +17,7 @@ if (!isset($_SESSION["username"])) {
 <html>
 <head>
 	<link rel="stylesheet" href="style.css">
+	<link rel="icon" type="image/png" href="logo-icon.png">
 	<title>Closet-Dex | Home</title>
 </head>
 
