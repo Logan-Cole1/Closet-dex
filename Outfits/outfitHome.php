a8bb137966511aae3493dc4e6ea93a658998244a
McCoy Ziehl <mvziehl@mtu.edu>
2025-04-16 13:34:49 -0400

    Add icon in title

--- Outfits/outfitHome.php
+++ Outfits/outfitHome.php
@@ -32,6 +32,7 @@ if (isset($_POST["randOutfit"])){
 <head>
 	<title>Closet-Dex | Wardrobe</title>
 	<link rel="stylesheet" href="../style.css">
+	<link rel="icon" type="image/png" href="../logo-icon.png">
 </head>
 
 <body>
