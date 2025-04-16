57ffde00fa7882d640fba3f48fe45ef0a7f235a9
McCoy Ziehl <mvziehl@mtu.edu>
2025-04-16 13:36:59 -0400

    Add icon in title correctly

--- Outfits/addOutfit.php
+++ Outfits/addOutfit.php
@@ -42,7 +42,7 @@ if (isset($_POST["addOutfit"])) {
 <head>
     <title>Closet-Dex | Add Outfit</title>
     <link rel="stylesheet" href="../style.css">
-    <link rel="icon" type="image/png" href="logo-icon.png">
+    <link rel="icon" type="image/png" href="../logo-icon.png">
 </head>
 <body>
     <p>Create an outfit:</p>
