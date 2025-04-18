d35be8f24650380995968524dae3a97e8b1caf5a
McCoy Ziehl <mvziehl@mtu.edu>
2025-04-18 13:06:10 -0400

    Redoing Head Changes

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ if (isset($_POST["register"])) {
 <head>
     <title>Closet-Dex | Login</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="logo-icon.png">
 </head>
 
 <body>
